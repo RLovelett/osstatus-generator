3ed4853e8d3b29a56d61f6d314485a0b6c5976b9
Ryan Lovelett <ryan@lovelett.me>
2022-12-23 11:50:15 -0500

    Help and documentation updates

--- Sources/osstatus-generator/osstatus_generator.swift
+++ Sources/osstatus-generator/osstatus_generator.swift
@@ -11,6 +11,20 @@ import RegexBuilder
 
 @main
 struct osstatus_generator: AsyncParsableCommand {
+    static var configuration = CommandConfiguration(
+        commandName: "osstatus-generator",
+        abstract: """
+A command-line interface (CLI) application that converts SecBase.h OSStatus
+codes into a Swift enum called OSStatusError.
+""",
+        discussion: """
+Provide a path, or use the default, to SecBase.h. This application will find all
+cases of the OSStatus enum along with their codes and possible description. The
+comments that appear after the cases of the OSStatus enum are used to create the
+descriptions.
+"""
+    )
+
     @Argument(help: "Path to SecBase.h", completion: .file())
     var secBase: URL = URL(filePath: "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Security.framework/Versions/A/Headers/SecBase.h")
 
